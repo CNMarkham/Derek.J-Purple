9579265addef6d984e986217f84331d9329f92c4
Write <markhamonca@codeninjas.com>
2025-08-07 19:50:05 -0400

    step 31

--- Derek.J - Pong/Assets/NewBehaviourScript.cs
+++ Derek.J - Pong/Assets/NewBehaviourScript.cs
@@ -8,14 +8,14 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow)) // "If the up arrow key is pressed..."
         {
-            transform.Translate(Vector2.up);
+            transform.Translate(Vector2.up * Time.deltaTime * speed); // "move up to the speed of Vector2.up * Time.deltaTime * speed"
         }
 
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow)) // "If the down arrow key is pressed..."
         {
-
+            transform.Translate(Vector2.down * Time.deltaTime * speed); // "move down to the speed of Vector2.down * Time.deltaTime * speed"
         }
     }
 }
