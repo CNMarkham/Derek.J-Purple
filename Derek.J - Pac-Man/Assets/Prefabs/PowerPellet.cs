2439e2971fb8fab96d8354b008c0ff04763d1b09
The chosen one <markhamonca@codeninjas.com>
2025-11-05 19:39:38 -0500

    step 63

---
+++ Derek.J - Pac-Man/Assets/Prefabs/PowerPellet.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerPellet : Pellet
+{
+    protected override void Eat()
+    {
+        base.Eat();
+
+        GameObject[] ghosts = GameObject.FindGameObjectsWithTag("Ghost");
+    }
+}
