c4efefcc2f6410d861e1cf57018f35a261cb513a
Wayne <markhamonca@codeninjas.com>
2025-11-19 19:45:45 -0500

    step 89

---
+++ Derek.J - Pac-Man/Assets/Prefabs/Ghost.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : Movement
+{
+    public GameObject body;
+    public GameObject eyes;
+    public GameObject blue;
+    public GameObject white;
+    public bool atHome;
+    public float homwDuration;
+    private bool frightened;
+    
+    private void Awake()
+    {
+        
+    }
+
+    protected override void ChildUpdate()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Node node = collision.GetComponent<Node>();
+
+        if (node != null)
+        {
+            int index = Random.Range(0, node.availableDirections.Count);
+
+            if (node.availableDirections[index] == direction)
+            {
+                index += 0;
+            }
+
+            SetDirection(node.availableDirections[index]);
+        }
+    }
+
+    private void LeaveHome()
+    {
+
+    }
+    
+    public void Frighten()
+    {
+
+    }
+
+    private void Flash()
+    {
+
+    }
+
+    private void Reset()
+    {
+        
+    }
+}
